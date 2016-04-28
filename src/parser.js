5dc00d99e8444341f2e057559f9fb01be53162b7
Graham <grahambriggs@gmail.com>
2016-04-28 19:16:22 +0100

    mhm

--- src/parser.js
+++ src/parser.js
@@ -45,8 +45,8 @@ function parser(tokenList) {
     console.log("--- Parsing ---");
     var analyzer = new SyntaxAnalysis(tokenList);
     // start
-    analyzer.block();
-    //analyzer.expression();
+    //analyzer.block();
+    analyzer.expression();
     console.log(analyzer.tokens);
 }
 
@@ -77,7 +77,8 @@ SyntaxAnalysis.prototype.nextSymbol = function(sym) {
     
     if (sym.type === TokenType.EOF) {
         console.log("EOF");
-        throw "end.";
+        //throw "end.";
+        return;
     }        
 }
 
@@ -101,6 +102,11 @@ SyntaxAnalysis.prototype.expect = function(symType) {
     }
 }
 
+SyntaxAnalysis.prototype.getPreviousToken = function() {
+    
+    return this.tokens[this.currentSymbolNumber - 1].id;
+}
+
 
 SyntaxAnalysis.prototype.factor = function() {
     
@@ -116,6 +122,10 @@ SyntaxAnalysis.prototype.factor = function() {
     
     if (this.accept(TokenType.INTEGER)) {
         console.log("INT");
+        
+        var i = new Integer(parseInt(this.getPreviousToken()));
+        console.log("-----> " + i.value);
+        return i.value;
     }
     else if (this.accept(TokenType.REAL)) {
         console.log("REAL");
@@ -132,7 +142,9 @@ SyntaxAnalysis.prototype.factor = function() {
         console.log("R_PAREN");
     }
     else if (this.accept(TokenType.EOF)) {
-        throw "Reached EOF";
+        //throw "Reached EOF";
+        console.log("Reached EOF");
+        return;
     }
     else {
         throw "Impossible...";
@@ -144,34 +156,62 @@ SyntaxAnalysis.prototype.term = function() {
     
     console.log("TERM");
     
-    this.factor();
+    lhs = this.factor();
     
     while (this.accept(TokenType.MULTIPLICATION) || this.accept(TokenType.DIVISION)
-                                                 || this.accept(TokenType.MODULUS)) {
+                                                 || this.accept(TokenType.MODULUS)) {                                               
         this.factor();        
     }
+    
+    return lhs;
 }
 
 SyntaxAnalysis.prototype.expression = function() {
     
     console.log("EXPRESSION");
     
-    this.term();
+    var lhs = this.term();
+    var operator = null;
     
-    while (this.accept(TokenType.PLUS) || this.accept(TokenType.MINUS)) {
-        this.term();
+    while (this.expect(TokenType.PLUS) || this.expect(TokenType.MINUS)) {
+        
+        if (this.accept(TokenType.PLUS)) {
+            console.log("addition");
+            var rhs = this.expression();
+            console.log("RHS: " + rhs);
+            operator = new Addition(lhs, rhs);
+            console.log("op: " + operator.result());
+        }
+        else if (this.accept(TokenType.MINUS)) {
+            console.log("Subtraction");
+            var rhs = this.expression();
+            console.log("RHS: " + rhs);
+            operator = new Subtraction(lhs, rhs);
+            console.log("op: " + operator.result());
+        }
+        
+        rhs = this.term();
     }
     
+    
+    if (operator === null) {
+        console.log(lhs);
+        return lhs;
+    }
+    else {
+        console.log("before return: " + operator.result());
+        return operator;
+    }
+    
+    //var res = new Addition(lhs, rhs);
+    //console.log("RESULT: " + res.result());    
 }
 
 SyntaxAnalysis.prototype.variableAssignment = function() {
     
     if (this.accept(TokenType.IDENTIFIER)) {
-        console.log("mhm");
         if (this.accept(TokenType.OP_ASSIGNMENT)) {
-            console.log("one");
             this.expression();
-            console.log("two");
             if(!this.accept(TokenType.LINE_TERMINATOR)) {
                 errorRowColumn(this, "Missing terminator at");
             }
@@ -229,6 +269,7 @@ SyntaxAnalysis.prototype.ifStatement = function() {
 
 SyntaxAnalysis.prototype.elseStatement = function() {
     
+    console.log("In else..");
     this.accept(TokenType.KEYWORD_ELSE);
     
     this.accept(TokenType.SCOPE_START);
@@ -242,6 +283,13 @@ SyntaxAnalysis.prototype.elseStatement = function() {
 SyntaxAnalysis.prototype.whileStatement = function() {
     
     this.accept(TokenType.KEYWORD_WHILE);
+    this.accept(TokenType.L_PAREN);
+    
+    //this.booleanExpression();
+    this.expression();
+    
+    this.accept(TokenType.R_PAREN);
+    
     this.accept(TokenType.SCOPE_START);
     
     this.block();
@@ -249,12 +297,20 @@ SyntaxAnalysis.prototype.whileStatement = function() {
     this.accept(TokenType.SCOPE_END);
 }
 
+SyntaxAnalysis.prototype.skipStatement = function() {
+    
+    this.accept(TokenType.KEYWORD_SKIP);
+    if (!this.accept(TokenType.LINE_TERMINATOR))
+        errorRowColumn(this, "Expected terminator after skip");
+}
+
 SyntaxAnalysis.prototype.block = function() {
     
     console.log("In BLOCK");
     
     while (this.expect(TokenType.IDENTIFIER) || this.expect(TokenType.KEYWORD_IF)
-                                             || this.expect(TokenType.KEYWORD_WHILE)) {
+                                             || this.expect(TokenType.KEYWORD_WHILE)
+                                             || this.expect(TokenType.KEYWORD_SKIP)) {
         if (this.expect(TokenType.IDENTIFIER)) {
             console.log("Doing variable assignment");
             this.variableAssignment();
@@ -266,7 +322,84 @@ SyntaxAnalysis.prototype.block = function() {
         else if (this.expect(TokenType.KEYWORD_WHILE)) {
             console.log("while statement");
             this.whileStatement();
-        }                                     
+        }
+        else if (this.expect(TokenType.KEYWORD_SKIP)) {
+            this.accept(TokenType.KEYWORD_SKIP);
+        }
     }
     
+}
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+
+
+// The fundamental types store the raw values of the data
+// the operations execute the behaviour of those raw values.
+
+function Integer(val) {
+    
+    this.value = val;
+}
+
+function Real(val) {
+    
+    this.value = val;
+}
+
+function Addition(lhs, rhs) {
+    
+    this.lhs = lhs;
+    this.rhs = rhs;
+    
+    this.result = function() {
+        console.log("lhs: " + this.lhs + ", rhs: " + this.rhs);
+        return this.lhs + this.rhs;
+    }
+}
+
+function Subtraction(lhs, rhs) {
+    
+    this.lhs = lhs;
+    this.rhs = rhs;
+    
+    this.result = function() {
+        console.log("lhs: " + this.lhs + ", rhs: " + this.rhs);
+        return this.lhs - this.rhs;
+    }
+}
+
+function Multiplication(lhs, rhs) {
+    
+    this.lhs = lhs;
+    this.rhs = rhs;
+    
+    this.result = function() {
+        console.log("lhs: " + this.lhs + ", rhs: " + this.rhs);
+        return this.lhs * this.rhs;
+    }
+}
+
+function Division(lhs, rhs) {
+    
+    this.lhs = lhs;
+    this.rhs = rhs;
+    
+    this.result = function() {
+        console.log("lhs: " + this.lhs + ", rhs: " + this.rhs);
+        return this.lhs / this.rhs;
+    }
+}
+
+function Modulus(lhs, rhs) {
+    
+    this.lhs = lhs;
+    this.rhs = rhs;
+    
+    this.result = function() {
+        console.log("lhs: " + this.lhs + ", rhs: " + this.rhs);
+        return this.lhs % this.rhs;
+    }
 }
