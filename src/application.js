2d6055a8685dd886740ba28ffe3dc27a425e81d2
Graham <grahambriggs@gmail.com>
2016-06-18 15:08:57 +0100

    frontend stuff

--- src/application.js
+++ src/application.js
@@ -1,6 +1,8 @@
 
 
 // Compiler entry point
+// Graham Briggs
+// June 2016
 
 
 function main(userCode) {
@@ -9,11 +11,14 @@ function main(userCode) {
 
 	var lexer = new Compiler.Lexer;
 	var parser = new Compiler.Parser;
-	//var irgen = new Compiler.IRGenerator;
+	var irgen = new Compiler.IRGenerator;
+
 	//var codegen = new Compiler.CodeGenerator;
 
 	lexer.scanner(userCode);
 	var tokens = lexer.getTokens();
+
+	updateLexerDiv(tokens);
 
 	parser.acceptTokens(tokens);
 	parser.startParser();
@@ -22,4 +27,20 @@ function main(userCode) {
 
 	console.log(ast);
 
+	var intermediateCode = irgen.traverseAST(ast);
+
+	//var asmcode = codegen.start(intermediateCode);
+
+}
+
+function updateLexerDiv(tokens) {
+
+	var tbl = "<div><table><tr><td>ID:</td><td>TYPE:</td><td>ROW</td><td>COLUMN</td></tr>";
+	for (var i = 0; i < tokens.length; i++) {
+		var tok = "<tr><td>" + tokens[i].id + "</td><td>" + Tokens.TokenString.getTokenString(tokens[i].type)
+		+ "</td><td>" + tokens[i].row + "</td><td>" + tokens[i].col + "</td></tr>";
+		tbl += tok;
+	}
+	tbl += "</table><div>";
+	document.getElementById("tokens").innerHTML = tbl;
 }
