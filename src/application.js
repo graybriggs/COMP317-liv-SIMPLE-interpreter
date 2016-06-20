0f93fe5207a555e6a5e8575c8e9b2374730c1820
Graham <grahambriggs@gmail.com>
2016-06-20 01:01:48 +0100

    html js webdev front end is annoying

--- src/application.js
+++ src/application.js
@@ -27,20 +27,36 @@ function main(userCode) {
 
 	console.log(ast);
 
-	var intermediateCode = irgen.traverseAST(ast);
-
+	irgen.traverseAST(ast);
+	var intermediateCode = irgen.getFinalIntermediateRepresentation();
+	console.log("IR:");
+	console.log(intermediateCode);
+	updateIRDiv(intermediateCode);
 	//var asmcode = codegen.start(intermediateCode);
 
 }
 
 function updateLexerDiv(tokens) {
 
-	var tbl = "<div><table><tr><td>ID:</td><td>TYPE:</td><td>ROW</td><td>COLUMN</td></tr>";
+	var tbl = "<div><table><tr><td>TOKEN DATA </td><td>TOKEN TYPE </td><td>ROW </td><td>COLUMN </td></tr>";
 	for (var i = 0; i < tokens.length; i++) {
 		var tok = "<tr><td>" + tokens[i].id + "</td><td>" + Tokens.TokenString.getTokenString(tokens[i].type)
 		+ "</td><td>" + tokens[i].row + "</td><td>" + tokens[i].col + "</td></tr>";
 		tbl += tok;
 	}
-	tbl += "</table><div>";
+	tbl += "</table></div>";
 	document.getElementById("tokens").innerHTML = tbl;
+}
+
+function updateIRDiv(ir) {
+
+	var tbl = "<div><table>";
+	for (var i = 0; i < ir.length; i++) {
+		tbl += "<tr><td>" + ir[i] + "</td></tr>";
+	}
+	tbl += "</table></div>";
+
+	console.log(tbl);
+
+	document.getElementById("ir").innerHTML = tbl;
 }
