37ad5e9bea7dd1a5cc1539ab948e173cf6923413
Graham <grahambriggs@gmail.com>
2016-07-03 00:53:08 +0100

    added prettyfier for IR labels; begina to make one for the asm instruction keyworks too. Not implemented yet though.

--- src/application.js
+++ src/application.js
@@ -12,28 +12,34 @@ function main(userCode) {
 	var lexer = new Compiler.Lexer;
 	var parser = new Compiler.Parser;
 	var irgen = new Compiler.IRGenerator;
+	var codegen = new Compiler.CodeGenerator;
 
-	//var codegen = new Compiler.CodeGenerator;
-
+////////////
 	lexer.scanner(userCode);
 	var tokens = lexer.getTokens();
-
 	updateLexerDiv(tokens);
+/////////
 
+//////////
 	parser.acceptTokens(tokens);
 	parser.startParser();
-
 	var ast = parser.getAbstractSyntaxTree();
-
 	console.log(ast);
+//////////
 
+//////////
 	irgen.traverseAST(ast);
 	var intermediateCode = irgen.getFinalIntermediateRepresentation();
 	console.log("IR:");
 	console.log(intermediateCode);
 	updateIRDiv(intermediateCode);
-	//var asmcode = codegen.start(intermediateCode);
+//////////
 
+//////////
+	codegen.startCodeGen(intermediateCode);
+	var asmcode = codegen.getGeneratedASM();
+	updateASMCodeDiv(asmcode);
+//////////
 }
 
 function updateLexerDiv(tokens) {
@@ -48,15 +54,60 @@ function updateLexerDiv(tokens) {
 	document.getElementById("tokens").innerHTML = tbl;
 }
 
+////////////////////////
+
+function prettyfyIR(ir) {
+	console.log(ir);
+	for (var i = 0; i < ir.length; i++) {
+		if (ir[i].indexOf(":") > -1) {
+			var line = ir[i];
+			line = "<b>" + line + "</b>";
+			console.log(line);
+			ir[i] = line;
+		}
+	}
+	return ir;
+}
+
 function updateIRDiv(ir) {
 
-	var tbl = "<div><table>";
+	ir = prettyfyIR(ir);
+	console.log(ir);
+	var tbl = "<div><center><table>";
 	for (var i = 0; i < ir.length; i++) {
 		tbl += "<tr><td>" + ir[i] + "</td></tr>";
 	}
-	tbl += "</table></div>";
-
-	console.log(tbl);
+	tbl += "</table></center></div>";
 
 	document.getElementById("ir").innerHTML = tbl;
+}
+
+///////////////////////
+
+function prettyfyASM(asmcode) {
+	
+	for (var i = 0; i < asmcode.length; i++) {
+		var pos = asmcode[i].indexOf(" ");
+		var line = asmcode[i].split(" ");
+
+		var updatedLine = "<font color=blue>" + line[0] + "</font>";
+
+		var ss = line.slice(pos, asmcode[i].length);
+		console.log(ss);
+		asmcode[i] = updatedLine + " " + ss;
+	}
+	return asmcode;
+}	
+
+function updateASMCodeDiv(generatedASM) {
+
+	//generatedASM = prettyfyASM(generatedASM);
+
+	var tbl = "<div><center><table>";
+	for (var i = 0; i < generatedASM.length; i++) {
+		tbl += "<tr><td>" + generatedASM[i] + "</td></tr>";
+	}
+	tbl += "</table></center></div>";
+
+	document.getElementById("codegen").innerHTML = tbl;
 }
